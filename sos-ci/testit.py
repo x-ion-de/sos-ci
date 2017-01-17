9d487ae45c8467ed31da94d1d312c44bf380c19b
Jens Rosenboom <j.rosenboom@x-ion.de>
2017-01-17 14:42:53 +0100

    Fix testit.py to work with the added depends_on parameter

--- sos-ci/testit.py
+++ sos-ci/testit.py
@@ -7,10 +7,12 @@ import executor
 
 
 # patchset is of the form 'refs/changes/26/111226/3'
+# depends_on is of the form 'openstack/cookbook-openstack-image:master:417834/3'
 # results_dir is of the form '/home/user/test-dir'
 
 patchset = str(sys.argv[1])
-results_dir = str(sys.argv[2])
+depends_on = str(sys.argv[2])
+results_dir = str(sys.argv[3])
 
 ref_name = patchset.replace('/', '-')
 
@@ -18,7 +20,7 @@ ref_name = patchset.replace('/', '-')
 results_dir = results_dir + '/' + ref_name
 os.mkdir(results_dir)
 
-(hash_id, success, results) = executor.just_doit(patchset, results_dir)
+(hash_id, success, results) = executor.just_doit(patchset, depends_on, results_dir)
 print "hash_id:%s", hash_id
 print "success:%s", success
 print "result:%s", results
